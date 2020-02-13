31d6413868ca02e71e5b3138e006f1e8a06725a0
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-02-13 20:58:40 -0300

    Configurando o git page

---
+++ build/precache-manifest.ea00f5457c0612e685997c0b0e5bb9b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcdbd8c128531087fe44724b3cde83f",
+    "url": "/my-movie-crud/index.html"
+  },
+  {
+    "revision": "72ddc734fca3ea4f7883",
+    "url": "/my-movie-crud/static/css/main.1a69d08f.chunk.css"
+  },
+  {
+    "revision": "37e25fb71b6405321792",
+    "url": "/my-movie-crud/static/js/2.d7462832.chunk.js"
+  },
+  {
+    "revision": "72ddc734fca3ea4f7883",
+    "url": "/my-movie-crud/static/js/main.8974cdb5.chunk.js"
+  },
+  {
+    "revision": "e0bf90edb4ec979779d8",
+    "url": "/my-movie-crud/static/js/runtime-main.f0497bf1.js"
+  }
+]);

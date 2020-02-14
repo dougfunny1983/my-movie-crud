e3737535b260ee030bf04a0ce2b49873bdf73ac6
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-02-13 21:42:26 -0300

    trocando os router

---
+++ build/precache-manifest.14ae0be2e97c9abad377faa034cc71d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13d283b0534e31a6c978ba02707de52",
+    "url": "/my-movie-crud/index.html"
+  },
+  {
+    "revision": "2521c314283acacd931a",
+    "url": "/my-movie-crud/static/css/main.1a69d08f.chunk.css"
+  },
+  {
+    "revision": "37e25fb71b6405321792",
+    "url": "/my-movie-crud/static/js/2.d7462832.chunk.js"
+  },
+  {
+    "revision": "2521c314283acacd931a",
+    "url": "/my-movie-crud/static/js/main.c5a1b69c.chunk.js"
+  },
+  {
+    "revision": "e0bf90edb4ec979779d8",
+    "url": "/my-movie-crud/static/js/runtime-main.f0497bf1.js"
+  }
+]);

10a892cd209c6ba604ba53de37eb86abb7af3917
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-02-13 21:58:35 -0300

    Updates

--- precache-manifest.636c3a6d5d24259a4cac425b19563866.js
+++ precache-manifest.636c3a6d5d24259a4cac425b19563866.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4d08b1f0b5a85f682eb06c246dc30fc1",
+    "revision": "88d93c8d739488587c41c2079a8793d8",
     "url": "/my-movie-crud/index.html"
   },
   {
-    "revision": "420aee680a936aaf841a",
+    "revision": "abc793bec55418e0de65",
     "url": "/my-movie-crud/static/css/main.1a69d08f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-movie-crud/static/js/2.d7462832.chunk.js"
   },
   {
-    "revision": "420aee680a936aaf841a",
-    "url": "/my-movie-crud/static/js/main.c0328ed0.chunk.js"
+    "revision": "abc793bec55418e0de65",
+    "url": "/my-movie-crud/static/js/main.c5a41193.chunk.js"
   },
   {
     "revision": "e0bf90edb4ec979779d8",
